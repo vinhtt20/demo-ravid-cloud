0c557643767e55d20199598eae390a115b70fd59
Quang Tran <214427629+vinhtt20@users.noreply.github.com>
2025-08-29 11:26:12 +0700

    Revert "Update terraform"
    This reverts commit a13fdc137953cf229639dbe4b9ad2a5149c3c576.

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,92 +1,40 @@
 terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.40"
+    required_version = ">= 1.5.0"
+    required_providers {
+        google = {
+        source  = "hashicorp/google"
+        version = "~> 5.40"
+        }
     }
-  }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+    project = var.project_id
+    region  = var.region
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.location
+    name     = var.cluster_name
+    location = var.location
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
+    remove_default_node_pool = true
+    initial_node_count       = 1
 
-  # Keep it public & simple (no private cluster, no extra IAM/VPC complexity)
-  ip_allocation_policy {}
+    # Keep it public & simple (no private cluster, no extra IAM/VPC complexity)
+    ip_allocation_policy {}
 }
 
-resource "google_container_node_pool" "primary_nodes_bluegreen" {
-  count = var.use_blue_green ? 1 : 0
+resource "google_container_node_pool" "primary_nodes" {
+    name     = "default-pool"
+    location = var.location
+    cluster  = google_container_cluster.primary.name
 
-  name     = "default-pool"
-  location = var.location
-  cluster  = google_container_cluster.primary.name
+    node_count = var.node_count
 
-  node_count = var.node_count
-
-  node_config {
-    machine_type = var.machine_type
-    disk_type    = var.disk_type
-    disk_size_gb = var.disk_size_gb
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-
-  upgrade_settings {
-    strategy = "BLUE_GREEN"
-
-    blue_green_settings {
-      node_pool_soak_duration = var.bg_soak_duration # ví dụ "0s" hoặc "300s"
-
-      standard_rollout_policy {
-        # Triển khai theo lô (batch)
-        batch_percentage    = var.bg_batch_percentage # 0.5 = 50% mỗi batch
-        batch_soak_duration = var.bg_batch_soak       # "0s" hoặc "120s"
-      }
+    node_config {
+        machine_type = var.machine_type
+        oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform"
+        ]
     }
-  }
-  timeouts {
-    update = var.update_timeout
-  }
 }
-
-resource "google_container_node_pool" "primary_nodes_surge" {
-  count    = var.use_blue_green ? 0 : 1
-
-  name     = "default-pool"
-  location = var.location
-  cluster  = google_container_cluster.primary.name
-
-  node_count = var.node_count
-
-  node_config {
-    machine_type = var.machine_type
-    disk_type    = var.disk_type
-    disk_size_gb = var.disk_size_gb
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-
-  upgrade_settings {
-    max_surge       = var.max_surge
-    max_unavailable = var.max_unavailable
-  }
-
-  timeouts {
-    update = var.update_timeout
-  }
-}
-
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,98 +1,31 @@
 variable "project_id" {
-  description = "GCP project ID"
-  type        = string
+    description = "GCP project ID"
+    type        = string
 }
 
 variable "region" {
-  description = "GCP region"
-  type        = string
-  default     = "asia-southeast1"
+    description = "GCP region"
+    type        = string
+    default     = "asia-southeast1"
 }
 
 variable "location" {
-  description = "Cluster location (zone or region). Use a zone for small test clusters."
-  type        = string
-  default     = "asia-southeast1-a"
+    description = "Cluster location (zone or region). Use a zone for a small test cluster."
+    type        = string
+    default     = "asia-southeast1-a"
 }
 
 variable "cluster_name" {
-  description = "Name of the GKE cluster"
-  type        = string
-  default     = "demo-ravid-cloud"
+    type    = string
+    default = "demo-ravid-cloud"
 }
 
 variable "node_count" {
-  description = "Number of nodes in the node pool"
-  type        = number
-  default     = 2
+    type    = number
+    default = 1
 }
 
 variable "machine_type" {
-  description = "Machine type for the GKE nodes"
-  type        = string
-  default     = "e2-standard-8"
-}
-
-variable "disk_type" {
-  description = "Disk type for nodes: pd-standard | pd-ssd | pd-balanced"
-  type        = string
-  default     = "pd-standard"
-}
-
-variable "disk_size_gb" {
-  description = "Disk size (in GB) for each node"
-  type        = number
-  default     = 100
-}
-
-variable "deletion_protection" {
-  description = "Enable/disable deletion protection for the cluster"
-  type        = bool
-  default     = false
-}
-
-# --- Rollout strategy selection ---
-variable "use_blue_green" {
-  description = "true = BLUE_GREEN rollout; false = SURGE rollout"
-  type        = bool
-  default     = true
-}
-
-# BLUE_GREEN tuning
-variable "bg_soak_duration" {
-  description = "Soak duration between Blue/Green phases, e.g., '0s' or '300s'"
-  type        = string
-  default     = "0s"
-}
-
-variable "bg_batch_percentage" {
-  description = "Batch percentage for BLUE_GREEN rollout [0..1], e.g., 0.5 = 50% per batch"
-  type        = number
-  default     = 0.5
-}
-
-variable "bg_batch_soak" {
-  description = "Soak duration between batches in BLUE_GREEN rollout, e.g., '0s' or '120s'"
-  type        = string
-  default     = "0s"
-}
-
-# SURGE tuning
-variable "max_surge" {
-  description = "Number of extra nodes to add temporarily during SURGE upgrades"
-  type        = number
-  default     = 1
-}
-
-variable "max_unavailable" {
-  description = "Number of nodes that can be unavailable during SURGE upgrades"
-  type        = number
-  default     = 0
-}
-
-# Timeouts
-variable "update_timeout" {
-  description = "Timeout for updating node pool (changing machine/disk may take a long time)"
-  type        = string
-  default     = "90m"
+    type    = string
+    default = "e2-standard-2"
 }
